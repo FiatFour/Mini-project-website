2e920f9089fccd87bb22b0339662556dec0a7cbc
FiatFour <fiatonetwo3@gmail.com>
2022-03-24 11:47:13 +0700

    first

---
+++ main.js
@@ -0,0 +1,21 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay: 200, origin: 'left'})
+sr.reveal('.container',{delay: 200, origin: 'top'})
