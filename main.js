5e3abbc35377721b14fb95b1dd2db7a2faf6c8d8
FiatFour <fiatonetwo3@gmail.com>
2022-04-06 14:21:18 +0700

    sure finished

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
-
 const sr = ScrollReveal ({
     distance: '60px',
     duration: 2500,
